ea644c0bd836eb5eaa25e59f4472d8117c815083
Victorious3 <vic@nightfall.moe>
2022-01-13 18:38:18 +0100

    Rest of the visitor methods

--- src/main/java/de/unituebingen/compilerbau/ast/ASTVisitor.java
+++ src/main/java/de/unituebingen/compilerbau/ast/ASTVisitor.java
@@ -1,11 +1,17 @@
 package de.unituebingen.compilerbau.ast;
 
+import de.unituebingen.compilerbau.ast.expression.Ternary;
 import de.unituebingen.compilerbau.ast.expression.binary.*;
 import de.unituebingen.compilerbau.ast.expression.bitwisebinary.*;
 import de.unituebingen.compilerbau.ast.expression.conditionaloperators.And;
 import de.unituebingen.compilerbau.ast.expression.conditionaloperators.Or;
-import de.unituebingen.compilerbau.ast.expression.relationaloperators.Equal;
-import de.unituebingen.compilerbau.ast.expression.relationaloperators.Greater;
+import de.unituebingen.compilerbau.ast.expression.relationaloperators.*;
+import de.unituebingen.compilerbau.ast.expression.unary.Negate;
+import de.unituebingen.compilerbau.ast.expression.unary.Not;
+import de.unituebingen.compilerbau.ast.statementexpressions.Assignment;
+import de.unituebingen.compilerbau.ast.statementexpressions.MethodCall;
+import de.unituebingen.compilerbau.ast.statementexpressions.New;
+import de.unituebingen.compilerbau.ast.statements.*;
 
 public interface ASTVisitor {
     void visit(Add add);
@@ -24,6 +30,25 @@ public interface ASTVisitor {
     void visit(ShiftRight shiftRight);
     void visit(UnsignedShiftRight unsignedShiftRight);
 
+    void visit(Ternary ternary);
+
     void visit(Equal equal);
     void visit(Greater greater);
+    void visit(GreaterOrEqual greaterOrEqual);
+    void visit(Less less);
+    void visit(LessOrEqual lessOrEqual);
+    void visit(NotEqual notEqual);
+
+    void visit(Negate negate);
+    void visit(Not not);
+
+    void visit(Assignment assignment);
+    void visit(MethodCall methodCall);
+    void visit(New _new);
+
+    void visit(For _for);
+    void visit(If _if);
+    void visit(LocalVarDeclaration localVarDeclaration);
+    void visit(Return _return);
+    void visit(While _while);
 }
