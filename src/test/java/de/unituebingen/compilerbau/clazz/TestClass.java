a0f802cfa3f0cfeea6f0fb229172b3db6c7cc53b
Erik Grosskopf <privat@MacBook-Pro-von-Erik.local>
2022-02-20 15:27:40 +0100

    Fix typos

--- src/test/java/de/unituebingen/compilerbau/clazz/TestClass.java
+++ src/test/java/de/unituebingen/compilerbau/clazz/TestClass.java
@@ -30,7 +30,7 @@ public class TestClass extends CompilerTest {
         Clazz mockClass = resultMap.get("MockClass");
 
         // TODO: Why is owner always null? Expected behaviour? Should this be done in TypeChecking?
-        // TODO: Why is type null here? The explicit type information would otherwise be lost otherwise, or?
+        // TODO: Why is type null here? The explicit type information would be lost otherwise, or?
         Field fieldA = new Field(null, PUBLIC, false, "a", null, null);
         Field fieldB = new Field(null, PUBLIC, false, "b", null, null);
         Field fieldC = new Field(null, PUBLIC, true, "c", null, null);
@@ -44,7 +44,7 @@ public class TestClass extends CompilerTest {
         fields.add(fieldE);
 
         Method test = new Method(PUBLIC, false, "test", new Type("void"), Collections.emptyMap(), new Block(Collections.emptyList()));
-        // TODO: Be consistent in turns of Type, use predefined Type if exists
+        // TODO: Be consistent in terms of Type, use predefined Type if exists - or use always string value
         Method test2 = new Method(PUBLIC, false, "test2", new Type("int"), Collections.emptyMap(), new Block(Arrays.asList(new Return(new IntLiteral(42)))));
         List<Method> methods = new ArrayList<>();
         methods.add(test);
