364336564ebd4ebe142fdfdc7f341f96de3c744d
Erik Grosskopf <erik.grosskopf@mail.de>
2022-02-22 23:33:22 +0100

    Adjust Template

--- TestTemplate.java
+++ TestTemplate.java
@@ -2,42 +2,55 @@ import de.unituebingen.compilerbau.CompilerTest;
 import de.unituebingen.compilerbau.ast.AccessModifier;
 import de.unituebingen.compilerbau.ast.Clazz;
 import de.unituebingen.compilerbau.exception.ASTException;
-import de.unituebingen.compilerbau.exception.CompilerException;
 import de.unituebingen.compilerbau.exception.TypeCheckException;
 import de.unituebingen.compilerbau.scanner.ScannerParser;
+import de.unituebingen.compilerbau.typing.TypeChecker;
 
+import java.io.IOException;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
-public class TestTemplate extends CompilerTest {
+public class TestEmptyClass extends CompilerTest {
+    @Override
     public String getMockFilePath() {
         return "/MockFolder/MockTemplate.java";
+    }
+
+    @Override
+    public Map<String, Clazz> getExpectedClassMap() {
+        final Clazz expectedAST = new Clazz(
+                AccessModifier.PUBLIC,
+                "MockTemplate",
+                Collections.emptyList(),
+                Collections.emptyList());
+
+        Map<String, Clazz> classMap = new HashMap<>();
+        classMap.put(expectedAST.name, expectedAST);
+
+        return classMap;
     }
 
     @Override
     public void testAST() throws ASTException {
         final ScannerParser scannerParser = new ScannerParser();
         Map<String, Clazz> resultMap = scannerParser.parse(this.getSourcecode());
-        Clazz mockEmptyClass = resultMap.get("MockEmptyClass");
+        Clazz mockEmptyClass = resultMap.get("MockTemplate");
 
-        final Clazz expectedAST = new Clazz(
-                AccessModifier.PUBLIC,
-                "MockEmptyClass",
-                Collections.emptyList(),
-                Collections.emptyList());
-
-        assertEquals(mockEmptyClass, expectedAST);
+        assertEquals(getExpectedClassMap().get("MockTemplate"), resultMap.get("MockTemplate"));
     }
 
     @Override
     public void testTypeCheckedAST() throws TypeCheckException {
-
+        TypeChecker typeChecker = new TypeChecker();
+        typeChecker.check(getExpectedClassMap());
     }
 
     @Override
-    public void testGeneratedBytecode() throws CompilerException {
-
+    public void testGeneratedBytecode() throws IOException, CloneNotSupportedException, ClassNotFoundException {
+        compileAndLoadClasses();
+        Class c = this.compiledClasses.get("MockTemplate");
     }
 }
